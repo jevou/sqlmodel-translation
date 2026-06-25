e0802087ab11a620371f9655076caccaa4fdc4d4
jevou <janerik@mailman.se>
2026-06-26 00:59:59 +0200

    example code for usage of inheritenace feature

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from sqlmodel import Field, Session, SQLModel, StaticPool, create_engine, select
 
 from modeltranslation import TranslationOptions, Translator, apply_translation
 
 engine = create_engine(
-    "sqlite://",
+    "sqlite:///db.sqlite",
     echo=True,
     connect_args={"check_same_thread": False},
     poolclass=StaticPool,
@@ -12,18 +12,21 @@ engine = create_engine(
 
 
 def create_db_and_tables() -> None:
+    SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
 
 
-class Book(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+class BookBase(SQLModel):
     title: str
     author: str
+class Book(BookBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
 
 
 translator = Translator(
     default_language="en",
     languages=("en", "pl"),
+    #fallback_languages={"default": ("pl",)},
 )
 
 
@@ -50,20 +53,29 @@ apply_translation(app, translator)
 
 
 @app.get("/all")
-def get_books() -> list[Book]:
+def get_books(
+        accept_language: str = Header(None),
+    ) -> list[Book]:
     with Session(engine) as session:
         return session.exec(select(Book)).all()
 
 
 @app.get("/titles")
-def get_titles() -> list[str]:
+def get_titles(
+        accept_language: str = Header(None),
+    ) -> list[str]:
     with Session(engine) as session:
         return session.exec(select(Book.title)).all()
 
 
 @app.post("/create")
-def create_book(book: Book) -> Book:
+def create_book(
+        book: Book,
+        accept_language: str = Header(None),
+    ) -> Book:
     with Session(engine) as session:
+        db_book = Book.model_validate(book)
+        print(db_book)
         session.add(book)
         session.commit()
         session.refresh(book)
